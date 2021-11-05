7d97279712eabeccfe1d7260d47e178584137c43
Henrique <henriqlustosa@gmail.com>
2021-11-05 13:45:02 -0300

    Classes de testes para os Usuarios

---
+++ src/test/java/br/desafio/livraria/repository/UsuarioRepositoryTest.java
@@ -0,0 +1,61 @@
+package br.desafio.livraria.repository;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import br.desafio.livraria.modelo.Usuario;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import br.desafio.livraria.mocks.UsuarioFactory;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles("test")
+public class UsuarioRepositoryTest {
+	 @Autowired
+	    private UsuarioRepository usuarioRepository;
+
+	    @Autowired
+	    private TestEntityManager testEntityManager;
+
+	    private Usuario usuario;
+	
+	 @BeforeEach
+	    private void setUp() {
+	        usuario = UsuarioFactory.criarUsuarioSemId();
+
+	        testEntityManager.persist(usuario);
+	    }
+
+	    @Test
+	    void findByEmailDeveriaRetornarUmUsuarioValido() {
+	        Optional<Usuario> usuarioEncontrado = usuarioRepository.findByLogin(usuario.getLogin());
+
+	        assertEquals(usuario.getId(), usuarioEncontrado.get().getId());
+	        assertEquals(usuario.getLogin(), usuarioEncontrado.get().getLogin());
+	        assertEquals(usuario.getNome(), usuarioEncontrado.get().getNome());
+	    }
+
+	    @Test
+	    void findByNaoDeveriaTerRetornoComEmailNaoCadastrado() {
+	        Optional<Usuario> usuarioEncontrado = usuarioRepository.findByLogin("henriqlustosa");
+
+	        assertTrue(usuarioEncontrado.isPresent());
+	    }
+
+}
---
+++ src/test/java/br/desafio/livraria/service/UsuarioServiceTest.java
@@ -0,0 +1,166 @@
+package br.desafio.livraria.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.desafio.livraria.dto.response.UsuarioDto;
+import br.desafio.livraria.dto.request.UsuarioFormDto;
+import br.desafio.livraria.dto.request.UsuarioUpdateFormDto;
+import br.desafio.livraria.exception.DomainException;
+import br.desafio.livraria.exception.ResourceNotFoundException;
+import br.desafio.livraria.mocks.UsuarioFactory;
+import br.desafio.livraria.modelo.Usuario;
+import br.desafio.livraria.repository.PerfilRepository;
+import br.desafio.livraria.repository.UsuarioRepository;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.persistence.EntityNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import java.util.Optional;
+@ExtendWith(MockitoExtension.class)
+public class UsuarioServiceTest {
+    @Mock
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    @Mock
+    private PerfilRepository perfilRepository;
+	
+	@Mock
+	private UsuarioRepository usuarioRepository ;
+	
+	@InjectMocks
+	private UsuarioService usuarioService;
+	
+	private Usuario usuario = UsuarioFactory.criarUsuario();
+
+	private UsuarioFormDto usuarioFormDto = UsuarioFactory.criarUsuarioFormDto();
+	private UsuarioUpdateFormDto usuarioUpdateFormComLoginDiferenteDto = UsuarioFactory
+	            .criarUsuarioUpdateFormComLoginDiferenteDto();
+	private UsuarioUpdateFormDto usuarioUpdateFormComMesmoLoginDto = UsuarioFactory
+	            .criarUsuarioUpdateFormComMesmoLoginDto();
+	
+	private UsuarioDto usuarioDto = UsuarioFactory.criarUsuarioResponseDto();
+	private UsuarioDto usuarioAtualizadoResponseDto = UsuarioFactory.criarUsuarioAtualizadoComMesmoLoginResponseDto();
+	private UsuarioDto   usuarioAtualizadoLoginDiferenteResponseDto = UsuarioFactory
+               .criarUsuarioAtualizadoComLoginDiferenteResponseDto();
+    @Test
+    void detalharDeveRetornarUsuarioDetalhado() {
+        long validId = 1L;
+        when(usuarioRepository.getById(anyLong())).thenReturn(usuario);
+        when(modelMapper.map(usuario, UsuarioDto.class)).thenReturn(usuarioDto);
+        UsuarioDto usuarioResponseDto = usuarioService.detalhar(validId);
+
+        assertEquals(validId, usuarioResponseDto.getId());
+        verify(usuarioRepository, times(1)).getById(validId);
+    }
+    @Test
+    void detalharDeveLancarResourceNotFoundExceptionQuandoIdInvalido() {
+        doThrow(EntityNotFoundException.class).when(usuarioRepository).getById(anyLong());
+
+        assertThrows(ResourceNotFoundException.class, () -> usuarioService.detalhar(1L));
+    }
+    @Test
+    void atualizarDeveLancarDomainExceptionQuandoUsuarioQuiserUtilizarLoginRegistrado() {
+    	  when(usuarioRepository.getById(anyLong())).thenReturn(usuario);
+          when(usuarioRepository.findByLogin(anyString())).thenReturn(Optional.of(usuario));
+
+          assertThrows(DomainException.class, () -> usuarioService.atualizar(usuarioUpdateFormComLoginDiferenteDto));
+          verify(usuarioRepository, times(0)).save(any());
+    }
+
+    @Test
+    void atualizarDeveLancarResourceNotFoundExceptionQuandoIdInvalido() {
+        doThrow(EntityNotFoundException.class).when(usuarioRepository).getById(anyLong());
+
+        assertThrows(ResourceNotFoundException.class,() -> usuarioService.atualizar(usuarioUpdateFormComLoginDiferenteDto));
+    }
+
+  
+
+    @Test
+    void atualizarDeveRetornarUsuarioAtualizadoComLoginDiferente() {
+        when(usuarioRepository.getById(anyLong())).thenReturn(usuario);
+        when(modelMapper.map(usuario, UsuarioDto.class)).thenReturn(usuarioAtualizadoLoginDiferenteResponseDto);
+        when(usuarioRepository.findByLogin(anyString())).thenReturn(null);
+        UsuarioDto usuarioAtualizado = usuarioService.atualizar(usuarioUpdateFormComLoginDiferenteDto);
+
+        assertEquals(usuarioAtualizado.getNome(), usuarioUpdateFormComLoginDiferenteDto.getNome());
+        verify(usuarioRepository, times(1)).save(any());
+    }
+
+    @Test
+    void atualizarDeveRetornarUsuarioAtualizadoComMesmoLogin() {
+        when(usuarioRepository.getById(anyLong())).thenReturn(usuario);
+        when(modelMapper.map(usuario, UsuarioDto.class)).thenReturn(usuarioAtualizadoResponseDto);
+        UsuarioDto usuarioAtualizado = usuarioService.atualizar(usuarioUpdateFormComMesmoLoginDto);
+
+        assertEquals(usuarioAtualizado.getNome(), usuarioUpdateFormComMesmoLoginDto.getNome());
+        verify(usuarioRepository, times(1)).save(any());
+    }
+    
+    
+    
+    
+    @Test
+    void deletarDeveLancarResourceNotFoundExceptionQuandoIdInvalido() {
+        doThrow(EmptyResultDataAccessException.class).when(usuarioRepository).deleteById(1L);
+
+        assertThrows(ResourceNotFoundException.class, () -> usuarioService.remover(1l));
+        verify(usuarioRepository, times(1)).deleteById(anyLong());
+    }
+    
+    @Test
+    void criarDeveriaRetornarUsuarioQuandoLoginNaoEstiverRegistrado() {
+    	
+    	when(usuarioRepository.save(Mockito.any(Usuario.class))).thenAnswer(i -> i.getArguments()[0]);
+        when(modelMapper.map(usuarioFormDto, Usuario.class)).thenReturn(usuario);
+        when(modelMapper.map(usuario, UsuarioDto.class)).thenReturn(usuarioDto);
+        UsuarioDto usuarioResponseDto = usuarioService.createUsuario(usuarioFormDto);
+
+        assertEquals(usuarioFormDto.getNome(), usuarioResponseDto.getNome());
+
+        assertEquals(usuarioFormDto.getLogin(), usuarioResponseDto.getLogin());
+       
+        verify(usuarioRepository, times(1)).save(any());
+    }
+    
+    
+    @Test
+    void deletarDeveLancarDomainExceptionQuandoUsuarioNaoPodeSerExcluido() {
+        doThrow(DataIntegrityViolationException.class).when(usuarioRepository).deleteById(1L);
+
+        assertThrows(DomainException.class, () -> usuarioService.remover(1l));
+        verify(usuarioRepository, times(1)).deleteById(anyLong());
+    }
+    
+    
+    @Test
+    void deletarNaoDeveRetornarNadaQuandoIdExistir() {
+        long validId = 1l;
+        usuarioRepository.deleteById(validId);
+
+        verify(usuarioRepository, times(1)).deleteById(validId);
+    }
+
+}
